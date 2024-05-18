f42579ddf5c50b3dff034520ecd8e0b819a535b3
anuroop <anuroopf02@gmail.com>
2024-05-19 01:23:36 +0530

    minor changes in firestore configurations

--- packages/firebase_repository/lib/src/firestore/firestore.dart
+++ packages/firebase_repository/lib/src/firestore/firestore.dart
@@ -2,3 +2,4 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 export 'interfaces/interfaces.dart';
 export 'implementations/implementations.dart';
+export 'model/firestore_base_model.dart';
--- packages/firebase_repository/lib/src/firestore/model/firestore_base_model.dart
+++ packages/firebase_repository/lib/src/firestore/model/firestore_base_model.dart
@@ -1,5 +1,5 @@
 abstract class FireStoreBaseModel{
-  Map<String,dynamic> toJson();
-  FireStoreBaseModel fromJson();
-  FireStoreBaseModel copyWith();
+  Map<String, dynamic> toJson();
+  FireStoreBaseModel fromJson(Map<String, dynamic> json);
+  FireStoreBaseModel copyWith(Map<String, dynamic> updates);
 }
